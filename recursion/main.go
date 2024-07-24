9bc02ddedab9c23d8928e59be917bf2af164d056
Rafael Bernardino <rafaelferreirab2@gmail.com>
2024-07-24 20:13:21 -0300

    docs: adds fibonacci output

--- recursion/main.go
+++ recursion/main.go
@@ -11,20 +11,20 @@ import "fmt"
 
 func main() {
 	for i := range 10 {
-		fmt.Printf("Result(%d) = %d\n", i, fibonacci(i))
+		fmt.Printf("result = %d\n", fibonacci(i))
 	}
 
 	/*
-	* Result(0) = 0
-	* Result(1) = 1
-	* Result(2) = 1
-	* Result(3) = 2
-	* Result(4) = 3
-	* Result(5) = 5
-	* Result(6) = 8
-	* Result(7) = 13
-	* Result(8) = 21
-	* Result(9) = 34
+	* result(0) = 0
+	* result(1) = 1
+	* result(2) = 1
+	* result(3) = 2
+	* result(4) = 3
+	* result(5) = 5
+	* result(6) = 8
+	* result(7) = 13
+	* result(8) = 21
+	* result(9) = 34
 	 */
 }
 
